aba54389c5411243db76872d910011aa474cdb81
yiyangii <19970606yyJmm>
2021-02-06 21:00:07 -0500

    recursion test

--- src/cis5511/TopSort/main.java
+++ src/cis5511/TopSort/main.java
@@ -3,22 +3,19 @@ import java.io.*;
 
 import java.util.Scanner;
 
-
-
 public class main {
-    static int n;
+    static int n = 0;
     //Initialization queue,linkedlist
+    static int calls = 0;
 
     static Queue bag = null;
     static LinkedList[] successor = null;
     private static int index;
-    private static int reversecount;
-    static int counter;
 
+    static int counter = 0;
+    static int [] pred = null; //pred array
+    static int [] output = null; //output array
 
-    static int [] pred = null;
-    static int [] output = null;
-    String str = null;
     static String numbers = null;
 
     public static void main(String[] args){
@@ -34,7 +31,6 @@ public class main {
             n = Integer.parseInt(str);
             //Create queue[n],Linkedlist[n],output array/counter array
             bag = new Queue();
-
             successor = new LinkedList[n];
             output = new int[n];
             pred = new int[n];
@@ -42,6 +38,7 @@ public class main {
             while((numbers = sc.nextLine())!= null) {
                 //seperate two number
                 String[] tok = numbers.split(" ");
+
                 int first = Integer.parseInt(tok[0]);
                 int second = Integer.parseInt(tok[1]);
                 //System.out.println(first + " " + second);
@@ -60,30 +57,14 @@ public class main {
                     successor[first - 1].add(second);
                 }
                 pred[second - 1]++;
-
             }
             /*for (int i = 0; i < n; i++) {
                 System.out.println(successor[i]);
-
-
             }
-
             for (int i = 0; i < n; i++) {
-
                 System.out.println(pred[i]);
-
             }
             */
-
-
-
-
-
-
-
-
-
-
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
@@ -92,85 +73,102 @@ public class main {
             if(pred[i] == 0){
                 bag.push(i+1);
                 //check bag with index 2/3/5
-                //System.out.println(i+1);
-
+                System.out.println(i+1);
             }
         }
         topsort();
-
-
-
+        System.out.println("calls"+calls);
     }
 
     private static void topsort() {
         int length = bag.length;
+        if(length > 0){
+            while (length > 0) {
+                //pop the last one of the bag
+                //         a. remove an object from the bag and output it in the output ranking;
+                //         b. for each successor of the output object, decrease its predecessor count by 1
+                //         if the predecessor count becomes zero, add the successor to the bag.
+                int predint = bag.pop();
+                int reversecount = 0;
+                //should print 2 3 5 as the front of the queue
+                //System.out.println(pred);
+                length--;
+                //add pred to the bag then we need to remove the connection between pred and successor
+                //Ranking 5
+                //System.out.println("The pred is " + predint);
+                //System.out.println("The success is " + successor[predint - 1]);
 
 
-        while (length > 0) {
-            //pop the last one of the bag
-            //         a. remove an object from the bag and output it in the output ranking;
-            //         b. for each successor of the output object, decrease its predecessor count by 1
-            //         if the predecessor count becomes zero, add the successor to the bag.
-            int predint = bag.pop();
-            //should print 2 3 5 as the front of the queue
-            //System.out.println(pred);
-            length--;
-            //add pred to the bag then we need to remove the connection between pred and successor
-            //Ranking 5
-            //System.out.println(predint);
-            output[index++] = predint;
+                output[counter++] = predint;
+                //System.out.println("The output is  " + predint);
+                //address the success
+                //3,4/  4,1/  5,4/  3,1/
+                //successor[] =1[]  2[]  3[4,1]  4[1] 5[4]
+                //0 0 1,4 1 4
+                //pred[i+1] = 1[2(4,3)]  2[0] 3[0] 4[2(5,3)] 5[0]
+                //2 0 0 2 0
+                //go to each successor of this pred cancel connection
+                //go to this pred in this case to find the successor of 2
+                //go to its counter of pred and do the descrment
+                if (successor[predint - 1] != null) {
+                    LinkedList.Node succ = successor[predint - 1].head;
+                    //successor[pred -1].head is the first element of successor set of pred
 
-            //address the success
-            //3,4/  4,1/  5,4/  3,1/
-            //successor[] =1[]  2[]  3[4,1]  4[1] 5[4]
-            //0 0 1,4 1 4
-            //pred[] = 1[2(4,3)]  2[0] 3[0] 4[2(5,3)] 5[0]
-            //2 0 0 2 0
-            //go to each successor of this pred cancel connection
-            //go to this pred in this case to find the successor of 2
-            //go to its
-            if (successor[predint - 1] != null) {
-                //successor[pred -1].head is the first element of successor set of pred
-                while (successor[predint - 1].head.next != null) {
-                    int s = successor[predint - 1].head.next.val;
-                    System.out.println(s);
-                    pred[s - 1]--;
-                    if (pred[s - 1] == 0) {
-                        //push back successor of 2
-                        bag.push(s);
-                        System.out.println(s);
-                        reversecount++;
+                    while (succ.next != null) {
+                        int s = succ.next.val;
+                        //System.out.println("The successor of predint is " + s);
 
+                        pred[s - 1]--;
+                        //use for recursion
+                        //if we check for the pred counter is already being decreased
+                        //then we push back and count how much push we have done
 
+                        if (pred[s - 1] == 0) {
+                            //push back successor
+                            bag.push(s);
+
+                            //System.out.println("Item we push to bag is " + s);
+                            //System.out.println(s);
+                            reversecount++;
+
+                        }
+                        //go to the next pred
+                        succ = succ.next;
                     }
-                    //go to the next pred
-                    successor[predint - 1].head.next = successor[predint - 1].head.next.next;
+
+
                 }
+                topsort();
 
-            }
-            topsort();
-            for (int i = 0; i < reversecount; i++) {
-                bag.remove_end();
-            }
-            bag.push(predint);
-
-            if (successor[predint - 1] != null) {
-                //successor[pred -1].head is the first element of successor set of pred
-                while (successor[predint - 1].head.next != null) {
-                    int s = successor[predint - 1].head.next.val;
-                    pred[s - 1]++;
-                    System.out.println(s);
-                    successor[predint - 1].head.next = successor[predint - 1].head.next.next;
+                for (int i = 0; i < reversecount; i++) {
+                    bag.remove_end();
+                    //System.out.println(bag.toString());
                 }
+                bag.push(predint);
 
+                if (successor[predint - 1] != null) {
+
+                    LinkedList.Node succ = successor[predint-1].head;
+
+                    while (succ.next != null) {
+                        int s = succ.next.val;
+                        pred[s - 1]++;
+
+                        succ = succ.next;
+                    }
+                }
+                counter--;
             }
-            counter--;
-
-
+        }else{
+            if (calls <= 25) {
+                //This takes O(n) time each time we want to print.
+                for (int i = 0; i < n; i++) {
+                    System.out.print(output[i] + " ");
+                }
+                System.out.println();
+            }
+            calls++;
         }
-
-
-
 
     }
 
